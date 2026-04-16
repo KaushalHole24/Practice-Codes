09e2e4563b62ad9049fe671fbb48a2af3478866b
Kaushal Hole <kaushalhole02@gmail.com>
2026-04-16 21:29:14 +0530

    .

--- MongoApp/index.js
+++ MongoApp/index.js
@@ -4,12 +4,15 @@ const port = 3000;
 const mongoose = require("mongoose");
 const path = require("path");
 const Chat = require("./models/chat");
+const methodOverride = require("method-override");
+const { matchesGlob } = require("path/posix");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+app.use(methodOverride("_method"));
 
 main()
     .then(()=>{
@@ -60,6 +63,23 @@ app.get("/chats/:id/edit", async (req, res)=>{
     let chat = await Chat.findById(id);
     res.render("edit", {chat});
 });
+
+// Update Route
+app.put("/chats/:id", async (req, res)=>{
+    let { id } = req.params;
+    let { msg } = req.body;
+    let updateChat = await Chat.findByIdAndUpdate(id, {msg: msg}, {new: true, runValidators: true});
+    console.log(updateChat);
+    res.redirect("/chats");
+});
+
+// Delete Route
+app.delete("/chats/:id", async(req, res)=>{
+    let {id} = req.params;
+    await Chat.findByIdAndDelete(id);
+    res.redirect("/chats");
+});
+
 
 app.listen(port, (req, res)=>{
     console.log("Listening at port 3000");
